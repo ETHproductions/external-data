a7539dd7821b91f22c351e913cf49214d8ed0cdd
ETHproductions <eth3792@nycap.rr.com>
2019-04-26 16:41:28 -0400

    Rework SWF blocks
    Should be much more flexible now.
    
    Before:
    - Play .swf at x,y for n seconds
      - Run code when finished
    
    After:
    - Play .swf at x,y
      - Run code when loaded/errored (with success check)
    - Close .swf (if it's playing)

--- PlaySWF.hx
+++ PlaySWF.hx
@@ -3,68 +3,104 @@ import com.stencyl.Engine;
 import openfl.display.Loader;
 import openfl.display.MovieClip;
 import openfl.events.Event;
+import openfl.utils.ByteArray;
 import flash.display.AVM1Movie;
 
 class PlaySWF {
+	
+	private static var moviesRunning:Map<String, Dynamic>;
 
 	public static function playSWF(filename:String, x:Float, y:Float, whenOver:Void->Void) {
-		playMovie(filename, x, y, 0, whenOver);
+		playMovie(filename, x, y, function(_) { whenOver(); });
 	}
 
 	public static function playAVM1(filename:String, ms:Float, whenOver:Void->Void) {
-		playMovie(filename, 0, 0, ms, whenOver);
+		playMovie(filename, 0, 0, function(_) { whenOver(); });
+		Timer.delay(function() { stopMovie(filename); }, Std.int(ms));
 	}
 	
 	#if (flash || display)
 	
-	public static function playMovie(filename:String, x:Float, y:Float, ms:Float, whenOver:Void->Void) {
+	public static function playMovie(filename:String, x:Float, y:Float, whenLoaded:Bool->Void) {
 		var clip:MovieClip;
 		var loader:Loader;
 		var filepath:String;
-		if (filename.substring(filename.length - 4, filename.length) == ".swf") {
-			filepath = "assets/data/" + filename;
-		} else {
-			filepath = "assets/data/" + filename + ".swf";
+		var bytes:ByteArray;
+		
+		if (filename.substr(-4) != ".swf") {
+			filename += ".swf";
 		}
-		var bytes = openfl.Assets.getBytes(filepath);
-		if (bytes == null) {
-			trace("SWF with name: " + filename + " does not exist");
+		filepath = "assets/data/" + filename;
+		
+		try {
+			bytes = openfl.Assets.getBytes(filepath);
+		} catch (e:Dynamic) {
+			trace("Could not find " + filename);
+			whenLoaded(false);
 			return;
 		}
+		
+		if (bytes == null) {
+			trace("Could not find " + filename);
+			whenLoaded(false);
+			return;
+		}
+		
 		loader = new Loader();
 		loader.loadBytes(bytes);
 		loader.contentLoaderInfo.addEventListener(Event.COMPLETE, function(_) {
 			if (Std.is(loader.content, AVM1Movie)) {
-				trace("Playing AVM1 SWF with name: " + filename);
+				// trace("Playing AVM1 SWF from file " + filename);
 				loader.x = x;
 				loader.y = y;
-				Engine.engine.root.addChild(loader);
-				
-				if (ms > 0)
-					Timer.delay(function() {
-						Engine.engine.root.removeChild(loader);
-						loader.unloadAndStop();
-						whenOver();
-					}, Std.int(ms));
+				startMovie(loader, filename);
 			} else {
-				trace("Playing AVM2 SWF with name: " + filename);
+				// trace("Playing AVM2 SWF from file " + filename);
 				clip = cast(loader.content, MovieClip);
 				clip.x = x;
 				clip.y = y;
-				Engine.engine.root.addChild(clip);
-				
-				if (ms > 0)
-					Timer.delay(function() {
-						Engine.engine.root.removeChild(clip);
-						whenOver();
-					}, Std.int(ms));
+				startMovie(clip, filename);
 			}
+			whenLoaded(true);
 		});
+	}
+	
+	public static function startMovie(movie:Dynamic, filename:String) {
+		if (moviesRunning == null) {
+			moviesRunning = new Map<String, Dynamic>();
+		}
+		
+		moviesRunning.set(filename, movie);
+		Engine.engine.root.addChild(movie);
+	}
+	
+	public static function stopMovie(filename:String) {
+		if (filename.substr(-4) != ".swf")
+			filename += ".swf";
+		
+		var movie:Dynamic = moviesRunning.get(filename);
+		if (movie == null) {
+			trace(filename + " is not currently running");
+			return;
+		}
+		
+		Engine.engine.root.removeChild(movie);
+		if (Std.is(movie, Loader))
+			movie.unloadAndStop();
+		
+		moviesRunning.remove(filename);
+		// trace("Closed SWF from file " + filename);
 	}
 	
 	#else
 	
-	public static function playMovie(filename:String, x:Float, y:Float, ms:Float, whenOver:Void->Void) {
+	public static function playMovie(filename:String, x:Float, y:Float, whenLoaded:Bool->Void) {
+		trace("Cannot play SWF files unless running on Flash.");
+	}
+	public static function startMovie(movie:Dynamic, filename:String) {
+		trace("Cannot play SWF files unless running on Flash.");
+	}
+	public static function stopMovie(filename:String) {
 		trace("Cannot play SWF files unless running on Flash.");
 	}
 	
